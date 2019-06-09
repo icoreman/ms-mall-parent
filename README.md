9fa8272a7d18e3b3c9068b55b87bbbaec65f8fc7
icoreman <296328799@qq.com>
2019-06-09 09:56:30 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ms-mall-parent
 A mall system build by Spring Cloud and Spring Boot。
+
+## [dubbo 版本](https://github.com/icoreman/mall)
+
+## [Config 仓库](https://github.com/icoreman/spring-cloud-mall-config)
---
+++ ms-mall-config-6001/src/main/java/com/xuxx/ms/mall/ConfigCenter6001_App.java
@@ -0,0 +1,22 @@
+package com.xuxx.ms.mall;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigCenter6001_App implements CommandLineRunner {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigCenter6001_App.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info(">>>>>>>>>>>>>>> 商城系统 配置中心 组件 启动完成<<<<<<<<<<<<<");
+	}
+}
